87c37fac9635385ebef5fed89e4c1cac70dcd94a
ryongmi <62281754+ryongmi@users.noreply.github.com>
2025-07-12 21:24:06 +0900

    feat(user-role) - entity 복합 인덱스 및 유니온 추가

--- src/modules/user-role/entities/user-role.entity.ts
+++ src/modules/user-role/entities/user-role.entity.ts
@@ -1,8 +1,9 @@
 import { Entity, Index, PrimaryColumn } from 'typeorm';
 
 @Entity('user_role')
-@Index('IDX_USER_ROLE_USER', ['userId']) // ✅ 사용자 기준 인덱스
-@Index('IDX_USER_ROLE_ROLE', ['roleId']) // ✅ 역할 기준 인덱스
+@Index('IDX_USER_ROLE_USER', ['userId'])
+@Index('IDX_USER_ROLE_ROLE', ['roleId'])
+@Index('IDX_USER_ROLE_UNIQUE', ['userId', 'roleId'], { unique: true })
 export class UserRoleEntity {
   @PrimaryColumn({ type: 'uuid' })
   userId!: string;
