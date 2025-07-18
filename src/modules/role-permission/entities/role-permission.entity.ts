395dbc25de2e4377e5fe2007cdb91e026797eecf
ryongmi <62281754+ryongmi@users.noreply.github.com>
2025-07-18 22:06:05 +0900

    feat - cores 설정 Header 추가

--- src/modules/role-permission/entities/role-permission.entity.ts
+++ src/modules/role-permission/entities/role-permission.entity.ts
@@ -1,13 +1,16 @@
-import { Entity, Index, PrimaryColumn } from 'typeorm';
+import { Entity, Index, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity('role_permission')
 @Index('IDX_ROLE_PERMISSION_ROLE', ['roleId']) // role 기준 조회
 @Index('IDX_ROLE_PERMISSION_PERMISSION', ['permissionId']) // permission 기준  -> 이건 나중에 필요없으면 지우는게 좋을지도
 @Index('IDX_ROLE_PERMISSION_UNIQUE', ['roleId', 'permissionId'], { unique: true }) // 중복 방지
 export class RolePermissionEntity {
-  @PrimaryColumn({ type: 'uuid' })
+  // @PrimaryColumn({ type: 'uuid' })
+  @PrimaryGeneratedColumn('uuid')
   roleId!: string;
 
-  @PrimaryColumn({ type: 'uuid' })
+  // @PrimaryColumn({ type: 'uuid' })
+  @PrimaryGeneratedColumn('uuid')
   permissionId!: string;
 }
+
--- src/modules/service-visible-role/entities/service-visible-role.entity.ts
+++ src/modules/service-visible-role/entities/service-visible-role.entity.ts
@@ -1,13 +1,16 @@
-import { Entity, Index, PrimaryColumn } from 'typeorm';
+import { Entity, Index, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity('service_visible_role')
 @Index('IDX_SVR_SERVICE', ['serviceId'])
 @Index('IDX_SVR_ROLE', ['roleId'])
 @Index('IDX_SVR_UNIQUE', ['serviceId', 'roleId'], { unique: true })
 export class ServiceVisibleRoleEntity {
-  @PrimaryColumn({ type: 'uuid' })
+  // @PrimaryColumn({ type: 'uuid' })
+  @PrimaryGeneratedColumn('uuid')
   serviceId!: string;
 
-  @PrimaryColumn({ type: 'uuid' })
+  // @PrimaryColumn({ type: 'uuid' })
+  @PrimaryGeneratedColumn('uuid')
   roleId!: string;
 }
+
