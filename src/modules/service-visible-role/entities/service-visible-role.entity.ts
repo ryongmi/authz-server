881d6f7092d042980b636f3a56a46a0bfd3fd704
ryongmi <62281754+ryongmi@users.noreply.github.com>
2025-07-20 05:54:46 +0900

    fix(entity) - 중간테이블 엔티티 pk 수정 - PrimaryGeneratedColumn -> PrimaryColumn

--- src/modules/service-visible-role/entities/service-visible-role.entity.ts
+++ src/modules/service-visible-role/entities/service-visible-role.entity.ts
@@ -1,16 +1,14 @@
-import { Entity, Index, PrimaryGeneratedColumn } from 'typeorm';
+import { Entity, Index, PrimaryColumn } from 'typeorm';
 
 @Entity('service_visible_role')
 @Index('IDX_SVR_SERVICE', ['serviceId'])
 @Index('IDX_SVR_ROLE', ['roleId'])
 @Index('IDX_SVR_UNIQUE', ['serviceId', 'roleId'], { unique: true })
 export class ServiceVisibleRoleEntity {
-  // @PrimaryColumn({ type: 'uuid' })
-  @PrimaryGeneratedColumn('uuid')
+  @PrimaryColumn({ type: 'uuid' })
   serviceId!: string;
 
-  // @PrimaryColumn({ type: 'uuid' })
-  @PrimaryGeneratedColumn('uuid')
+  @PrimaryColumn({ type: 'uuid' })
   roleId!: string;
 }
 
--- src/modules/user-role/entities/user-role.entity.ts
+++ src/modules/user-role/entities/user-role.entity.ts
@@ -1,16 +1,14 @@
-import { Entity, Index, PrimaryGeneratedColumn } from 'typeorm';
+import { Entity, Index, PrimaryColumn } from 'typeorm';
 
 @Entity('user_role')
 @Index('IDX_USER_ROLE_USER', ['userId'])
 @Index('IDX_USER_ROLE_ROLE', ['roleId'])
 @Index('IDX_USER_ROLE_UNIQUE', ['userId', 'roleId'], { unique: true })
 export class UserRoleEntity {
-  // @PrimaryColumn({ type: 'uuid' })
-  @PrimaryGeneratedColumn('uuid')
+  @PrimaryColumn({ type: 'uuid' })
   userId!: string;
 
-  // @PrimaryColumn({ type: 'uuid' })
-  @PrimaryGeneratedColumn('uuid')
+  @PrimaryColumn({ type: 'uuid' })
   roleId!: string;
 }
 
