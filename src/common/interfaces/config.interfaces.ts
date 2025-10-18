af6aecbc63e272700526d430502de3141529e84f
ryongmi <62281754+ryongmi@users.noreply.github.com>
2025-10-18 23:18:00 +0900

    fix - 사용하지 않는 config interface 주석처리

--- src/common/interfaces/config.interfaces.ts
+++ src/common/interfaces/config.interfaces.ts
@@ -1,9 +1,14 @@
-import type { StringValue } from 'ms';
-
 export interface DefaultConfig {
   mode: 'local' | 'development' | 'production' | undefined;
   port: number | undefined;
   corsOrigins: string | undefined;
+}
+
+export interface ClientConfig {
+  authServiceHost: string | undefined;
+  authServicePort: number | undefined;
+  portalServiceHost: string | undefined;
+  portalServicePort: number | undefined;
 }
 
 export interface MysqlConfig {
@@ -22,40 +27,6 @@ export interface RedisConfig {
   password: string | undefined;
 }
 
-export interface GoogleConfig {
-  clientId: string | undefined;
-  clientSecret: string | undefined;
-  redirectUrl: string | undefined;
-  tokenUrl: string | undefined;
-  userInfoUrl: string | undefined;
-}
-
-export interface NaverConfig {
-  clientId: string | undefined;
-  clientSecret: string | undefined;
-  redirectUrl: string | undefined;
-  tokenUrl: string | undefined;
-  userInfoUrl: string | undefined;
-}
-
-export interface ClientConfig {
-  authServiceHost: string | undefined;
-  authServicePort: number | undefined;
-  portalServiceHost: string | undefined;
-  portalServicePort: number | undefined;
-}
-
 export interface JwtConfig {
-  accessPrivateKey: string | undefined;
   accessPublicKey: string | undefined;
-  refreshPrivateKey: string | undefined;
-  refreshPublicKey: string | undefined;
-  accessExpiresIn: StringValue | undefined;
-  refreshExpiresIn: StringValue | undefined;
-  sessionCookiePath: string | undefined;
-  refreshMaxAge: number | undefined;
-  refreshStore: string | undefined;
-  blackListStore: string | undefined;
-  naverStateStore: string | undefined;
-  googleStateStore: string | undefined;
 }
